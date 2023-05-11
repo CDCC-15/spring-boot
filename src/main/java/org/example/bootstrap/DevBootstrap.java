4f0643b49d08f493a81ff566c9173fa5d88b8683
Cristhian Canizares <cdcccr7@gmail.com>
2023-05-11 15:19:31 -0400

    Added default values

--- src/main/java/org/example/bootstrap/DevBootstrap.java
+++ src/main/java/org/example/bootstrap/DevBootstrap.java
@@ -1,19 +1,23 @@
 package org.example.bootstrap;
 
-import org.example.model.Category;
+import org.example.model.*;
 import org.example.repositories.*;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
-    private CategoryRepository categoryRepository;
-    private ContractRepository contractRepository;
-    private EmployeeRepository employeeRepository;
-    private PositionRepository positionRepository;
-    private ItemRepository itemRepository;
-    private SubCategoryRepository subCategoryRepository;
+    private final CategoryRepository categoryRepository;
+    private final ContractRepository contractRepository;
+    private final EmployeeRepository employeeRepository;
+    private final PositionRepository positionRepository;
+    private final ItemRepository itemRepository;
+    private final SubCategoryRepository subCategoryRepository;
 
     public DevBootstrap(CategoryRepository categoryRepository,
                         ContractRepository contractRepository,
@@ -35,11 +39,55 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData() {
-        // PPE - Personal Protection Equipment
         Category eppCategory = new Category();
         eppCategory.setCode("EPP");
         eppCategory.setName("EPP");
-
         categoryRepository.save(eppCategory);
+
+        Category resourceCategory = new Category();
+        resourceCategory.setCode("RES");
+        resourceCategory.setName("RESOURCE");
+        categoryRepository.save(resourceCategory);
+
+        SubCategory rawMaterialSubCategory = new SubCategory();
+        rawMaterialSubCategory.setCategory(resourceCategory);
+        rawMaterialSubCategory.setCode("RM");
+        rawMaterialSubCategory.setName("RAW MATERIAL");
+        subCategoryRepository.save(rawMaterialSubCategory);
+
+        SubCategory safetySubCategory = new SubCategory();
+        safetySubCategory.setCategory(eppCategory);
+        safetySubCategory.setCode("SAF");
+        safetySubCategory.setName("SAFETY");
+        subCategoryRepository.save(safetySubCategory);
+
+        Item helmet = new Item();
+        helmet.setCode("HEL");
+        helmet.setName("HELMET");
+        helmet.setSubCategory(safetySubCategory);
+        itemRepository.save(helmet);
+
+        Item ink = new Item();
+        ink.setCode("INK");
+        ink.setName("INK");
+        ink.setSubCategory(rawMaterialSubCategory);
+        itemRepository.save(ink);
+
+        Employee john = new Employee();
+        john.setFirstName("John");
+        john.setLastName("Doe");
+
+        Position position = new Position();
+        position.setName("OPERATIVE");
+        positionRepository.save(position);
+
+        Contract contract = new Contract();
+        contract.setEmployee(john);
+        contract.setInitDate(LocalDate.of(2023, 1,1));
+        contract.setPosition(position);
+
+        john.getContracts().add(contract);
+        employeeRepository.save(john);
+        contractRepository.save(contract);
     }
 }
